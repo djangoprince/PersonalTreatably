94a4dd3114b2f2b8751e7bd2819d12d6574e4e12
djangoprince <57494042+djangoprince@users.noreply.github.com>
2020-10-05 05:02:45 +0000

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Treatably
+//
+//  Created by Safa Jemai on 04/10/2020.
+//  Copyright © 2020 Treatably. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+
+}
+
