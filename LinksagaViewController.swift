6bd93e472c87516c2cf007685c32ac1f256d42c3
djangoprince <57494042+djangoprince@users.noreply.github.com>
2020-10-05 06:56:38 +0000

    Update LinksagaViewController.swift
    this class get the information of the user from the platform Saga into Treatably.

--- LinksagaViewController.swift
+++ LinksagaViewController.swift
@@ -2,7 +2,7 @@
 //  LinksagaViewController.swift
 //  Treatably
 //
-//  Created by Safa Jemai on 05/10/2020.
+//  Created by Safa Jemai on 05/10/2020. 
 //  Copyright © 2020 Treatably. All rights reserved.
 //
 
